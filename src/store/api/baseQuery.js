accb1bf851a4b9b80d30f734b8baddc5238b6d99
jaydevkalariyaa <jaydev.k@crestskillserve.com>
2025-09-24 22:23:19 +0530

    changes done

--- src/store/api/baseQuery.js
+++ src/store/api/baseQuery.js
@@ -1,15 +1,15 @@
 import { fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { logout } from '../slices/authSlice';
-
+const API_URL = import.meta.env.VITE_API_BASE_URL;
 // Custom base query that handles 401 responses
 export const baseQueryWithReauth = async (args, api, extraOptions) => {
   let result = await fetchBaseQuery({
-    baseUrl: 'http://127.0.0.1:8000/api',
+    baseUrl: API_URL,
     prepareHeaders: (headers) => {
       // Add auth token if available
-      const token = localStorage.getItem('token');
+      const token = localStorage.getItem("token");
       if (token) {
-        headers.set('authorization', `Bearer ${token}`);
+        headers.set("authorization", `Bearer ${token}`);
       }
       return headers;
     },
